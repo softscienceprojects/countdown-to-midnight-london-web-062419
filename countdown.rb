c9bda52e029ce48089f4a5583ab5b422a8f0ec54
erin johnson <erin.nicole.johnson@gmail.com>
2019-05-11 13:31:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(integer)
   counter = integer
   while counter > 0
-  puts "#{number} SECOND(S)!"
-  counter -= 1
-  "HAPPY NEW YEAR!"
-end
+    puts "#{number} SECOND(S)!"
+    counter -= 1
+    "HAPPY NEW YEAR!"
+  end
 end
